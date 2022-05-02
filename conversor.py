9cf8bf452f9cd2ecb70a93c3e47a979e3cc974a2
liosxx <ybarraroberto82@gmail.com>
2022-05-02 06:55:11 -0400

    Se agrgo la funcion run()

--- conversor.py
+++ conversor.py
@@ -1,41 +1,46 @@
-menu = """
-Bienvenido al conversor de moneda 🐧
+def run():
+    menu = """
+    Bienvenido al conversor de moneda 🐧
 
-1 - Pesos colombiano
-2 - Bolivares
-3 - Euros
+    1 - Pesos colombiano
+    2 - Bolivares
+    3 - Euros
 
-Elije una opcion (1, 2, 3): """
+    Elije una opcion (1, 2, 3): """
 
-#Este es el conversador de peso
-opcion = int(input(menu))
-if opcion == 1:
-    pesos = input("Cuanto pesos tienes: ")
-    pesos = float(pesos)
-    valor_dolar = 0.00026
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes: " + dolares + "$")
+    #Este es el conversador de peso
+    opcion = int(input(menu))
+    if opcion == 1:
+        pesos = input("Cuanto pesos tienes: ")
+        pesos = float(pesos)
+        valor_dolar = 0.00026
+        dolares = pesos / valor_dolar
+        dolares = round(dolares, 2)
+        dolares = str(dolares)
+        print("Tienes: " + dolares + "$")
 
-#Este es el conversor de bolivares
-elif opcion == 2:
-    bolivar = input("Cuanto bolivares tienes: ")
-    bolivar = float(bolivar)
-    valor_dolar = 4.43
-    dolares = bolivar / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes: " + dolares + "$")
-    
-#Este es el conversor
-elif opcion == 3:
-    euro = input("Cuanto euro tienes: ")
-    euro = float(euro)
-    valor_dolar = 1.08
-    dolares = euro / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes: " + dolares + "$")
-else:
-    print("La opcion ingresada es incorrecta") 
+    #Este es el conversor de bolivares
+    elif opcion == 2:
+        bolivar = input("Cuanto bolivares tienes: ")
+        bolivar = float(bolivar)
+        valor_dolar = 4.43
+        dolares = bolivar / valor_dolar
+        dolares = round(dolares, 2)
+        dolares = str(dolares)
+        print("Tienes: " + dolares + "$")
+        
+    #Este es el conversor
+    elif opcion == 3:
+        euro = input("Cuanto euro tienes: ")
+        euro = float(euro)
+        valor_dolar = 1.08
+        dolares = euro / valor_dolar
+        dolares = round(dolares, 2)
+        dolares = str(dolares)
+        print("Tienes: " + dolares + "$")
+    else:
+        print("La opcion ingresada es incorrecta") 
+
+
+if __name__ == "__main__":
+    run()
--- edad.py
+++ edad.py
@@ -1,5 +1,10 @@
-edad = int(input("Ingresa tu edad, para saber si eres mayor o menor: "))
-if edad > 17:
-    print("Guao eres mayor de edad")
-else:
-    print("Mmm, sigues siendo menor")
+def run():
+    edad = int(input("Ingresa tu edad, para saber si eres mayor o menor: "))
+    if edad > 17:
+        print("Guao eres mayor de edad")
+    else:
+        print("Mmm, sigues siendo menor")
+
+
+if __name__ == "__main__":
+    run()
